674c8b1cf1069285aa2dae7fe40212cd198bca09
qbandres <qbandres@gmail.com>
2025-02-03 16:57:32 -0500

    Seguimso con el arrgleo de env

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,3 +1,5 @@
+from typing import List
+from pydantic import validator
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
@@ -6,9 +8,16 @@ class Settings(BaseSettings):
     db_host: str
     db_port: int
     db_name: str
+    # Definimos allowed_origins como una lista de strings con un valor por defecto para desarrollo.
+    allowed_origins: List[str] = ["http://localhost:5173"]
+
+    @validator("allowed_origins", pre=True)
+    def split_allowed_origins(cls, value):
+        if isinstance(value, str):
+            return [origin.strip() for origin in value.split(",")]
+        return value
 
     class Config:
         env_file = ".env"  # Archivo que contiene las variables de entorno
-
 
 settings = Settings()
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,18 +1,13 @@
 import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from app.config import settings  # Asegúrate de que la ruta sea correcta
 
 app = FastAPI()
 
-# Valor por defecto para desarrollo que incluye localhost.
-default_origins = "http://localhost:5173"
-# de lo contrario, se usará el valor por defecto.
-allowed_origins_str = os.getenv("ALLOWED_ORIGINS", default_origins)
-allowed_origins = allowed_origins_str.split(",")
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=allowed_origins,
+    allow_origins=settings.allowed_origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
