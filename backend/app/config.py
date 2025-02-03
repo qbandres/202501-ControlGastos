8b7be4f3e08b7467612da8a332734852e259a6c4
qbandres <qbandres@gmail.com>
2025-02-03 17:55:37 -0500

    >env de backend corregido

--- backend/app/config.py
+++ backend/app/config.py
@@ -6,6 +6,8 @@ class Settings(BaseSettings):
     db_host: str
     db_port: int
     db_name: str
+    # Definimos allowed_origins como un string
+    allowed_origins: str
 
     class Config:
         env_file = ".env"  # Archivo que contiene las variables de entorno
